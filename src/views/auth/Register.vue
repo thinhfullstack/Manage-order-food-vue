569e6bebf99ac456c6a8f95734c28baf06831ac4
tranvanthinh <xuanloc23082000@gmail.com>
2023-06-07 09:22:44 +0700

    register type user

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -37,39 +37,39 @@
                                             団体名
                                         </div>
                                         <input type="text"
-                                            v-model="state.registerForm.organizationName"
-                                            v-bind:class="v$.organizationName.$errors == !v$.organizationName.$errors ? 'success' : 'error'"
+                                            v-model="state.registerForm.organization_company"
+                                            v-bind:class="v$.organization_company.$errors == !v$.organization_company.$errors ? 'success' : 'error'"
                                             @blur="v$.$touch()"
                                             name=""
                                             placeholder="株式会社〇〇"
                                         >
-                                            <span class="invalid-feedback" v-for="error of v$.organizationName.$errors" :key="error.$uid">{{ error.$message }}</span>
+                                            <span class="invalid-feedback" v-for="error of v$.organization_company.$errors" :key="error.$uid">{{ error.$message }}</span>
                                     </div>
                                     <div class="form_box">
                                         <div class="form_headline">
                                         所属（引渡し先）
                                         </div>
                                         <input type="text" 
-                                            v-model="state.registerForm.affiliates"
-                                            v-bind:class="v$.affiliates.$errors == !v$.affiliates.$errors ? 'success' : 'error'"
+                                            v-model="state.registerForm.affiliates_company"
+                                            v-bind:class="v$.affiliates_company.$errors == !v$.affiliates_company.$errors ? 'success' : 'error'"
                                             @blur="v$.$touch()"
                                             name=""
                                             placeholder="営業部"
                                         >
-                                            <span class="invalid-feedback" v-for="error of v$.affiliates.$errors" :key="error.$uid">{{ error.$message }}</span>
+                                            <span class="invalid-feedback" v-for="error of v$.affiliates_company.$errors" :key="error.$uid">{{ error.$message }}</span>
                                     </div>
                                     <div class="form_box">
                                         <div class="form_headline">
                                         管理者名
                                         </div>
                                         <input type="text"
-                                            v-model="state.registerForm.name"
-                                            v-bind:class="v$.name.$errors == !v$.name.$errors ? 'success' : 'error'"
+                                            v-model="state.registerForm.company_name"
+                                            v-bind:class="v$.company_name.$errors == !v$.company_name.$errors ? 'success' : 'error'"
                                             @blur="v$.$touch()"
                                             name=""
                                             placeholder="山田　太郎"
                                         >
-                                            <span class="invalid-feedback" v-for="error of v$.name.$errors" :key="error.$uid">{{ error.$message }}</span>
+                                            <span class="invalid-feedback" v-for="error of v$.company_name.$errors" :key="error.$uid">{{ error.$message }}</span>
                                     </div>
                                     <div class="form_box">
                                         <div class="form_headline">
@@ -78,26 +78,26 @@
                                         <div class="flex_wrap zip_frame">
                                             <div>
                                                 <input type="text"
-                                                    v-model="state.registerForm.zipCode1"
-                                                    v-bind:class="v$.zipCode1.$errors == !v$.zipCode1.$errors ? 'success' : 'error'"
+                                                    v-model="state.registerForm.zip_code_1"
+                                                    v-bind:class="v$.zip_code_1.$errors == !v$.zip_code_1.$errors ? 'success' : 'error'"
                                                     @blur="v$.$touch()"
                                                     name="zip31" 
                                                     maxlength="3" 
                                                     placeholder="000"
                                                 >
-                                                    <span class="invalid-feedback" v-for="error of v$.zipCode1.$errors" :key="error.$uid">{{ error.$message }}</span>
+                                                    <span class="invalid-feedback" v-for="error of v$.zip_code_1.$errors" :key="error.$uid">{{ error.$message }}</span>
                                             </div>
                                             <div>
                                                 <input type="text" 
-                                                    v-model="state.registerForm.zipCode2"
-                                                    v-bind:class="v$.zipCode2.$errors == !v$.zipCode2.$errors ? 'success' : 'error'"
+                                                    v-model="state.registerForm.zip_code_2"
+                                                    v-bind:class="v$.zip_code_2.$errors == !v$.zip_code_2.$errors ? 'success' : 'error'"
                                                     @blur="v$.$touch()"
                                                     name="zip32" 
                                                     maxlength="4" 
                                                     onKeyUp="AjaxZip3.zip2addr('zip31','zip32','pref','pref','addr1');" 
                                                     placeholder="0000"
                                                 >
-                                                    <span class="invalid-feedback" v-for="error of v$.zipCode2.$errors" :key="error.$uid">{{ error.$message }}</span>
+                                                    <span class="invalid-feedback" v-for="error of v$.zip_code_2.$errors" :key="error.$uid">{{ error.$message }}</span>
                                             </div>
                                         </div>
                                     </div>
@@ -164,27 +164,39 @@
                                         団体名
                                         </div>
                                         <input type="text" 
+                                            v-model="state.registerForm.organization_school"
+                                            v-bind:class="v$.organization_school.$errors == !v$.organization_school.$errors ? 'success' : 'error'"
+                                            @blur="v$.$touch()"
                                             name="" 
                                             placeholder="〇〇中学校"
                                         >
+                                        <span class="invalid-feedback" v-for="error of v$.organization_school.$errors" :key="error.$uid">{{ error.$message }}</span>
                                     </div>
                                     <div class="form_box">
                                         <div class="form_headline">
                                         所属（学年・クラス）
                                         </div>
-                                        <input type="text" 
+                                        <input type="text"
+                                            v-model="state.registerForm.affiliates_school"
+                                            v-bind:class="v$.affiliates_school.$errors == !v$.affiliates_school.$errors ? 'success' : 'error'"
+                                            @blur="v$.$touch()"
                                             name="" 
                                             placeholder="〇年〇組"
                                         >
+                                        <span class="invalid-feedback" v-for="error of v$.affiliates_school.$errors" :key="error.$uid">{{ error.$message }}</span>
                                     </div>
                                     <div class="form_box">
                                         <div class="form_headline">
                                         管理者名
                                         </div>
-                                        <input type="text" 
+                                        <input type="text"
+                                            v-model="state.registerForm.school_name"
+                                            v-bind:class="v$.school_name.$errors == !v$.school_name.$errors ? 'success' : 'error'"
+                                            @blur="v$.$touch()"
                                             name="" 
                                             placeholder="山田太郎"
                                         >
+                                        <span class="invalid-feedback" v-for="error of v$.school_name.$errors" :key="error.$uid">{{ error.$message }}</span>
                                     </div>
                                     <div class="form_box">
                                         <div class="form_headline">
@@ -193,20 +205,26 @@
                                         <div class="flex_wrap zip_frame">
                                             <div>
                                                 <input type="text" 
-                                                
+                                                    v-model="state.registerForm.zip_code_1"
+                                                    v-bind:class="v$.zip_code_1.$errors == !v$.zip_code_1.$errors ? 'success' : 'error'"
+                                                    @blur="v$.$touch()"
                                                     name="zip31" 
                                                     maxlength="3" 
                                                     placeholder="000"
                                                 >
+                                                <span class="invalid-feedback" v-for="error of v$.zip_code_1.$errors" :key="error.$uid">{{ error.$message }}</span>
                                             </div>
                                             <div>
                                                 <input type="text" 
-
+                                                    v-model="state.registerForm.zip_code_2"
+                                                    v-bind:class="v$.zip_code_2.$errors == !v$.zip_code_2.$errors ? 'success' : 'error'"
+                                                    @blur="v$.$touch()"
                                                     name="zip32" 
                                                     maxlength="4" 
                                                     onKeyUp="AjaxZip3.zip2addr('zip31','zip32','pref','pref','addr1');" 
                                                     placeholder="0000"
                                                 >
+                                                <span class="invalid-feedback" v-for="error of v$.zip_code_2.$errors" :key="error.$uid">{{ error.$message }}</span>
                                             </div>
                                         </div>
                                     </div>
@@ -216,37 +234,52 @@
                                         </div>
                                         <div name="pref"></div>
                                         <input type="text" 
-
+                                            v-model="state.registerForm.address"
+                                            v-bind:class="v$.address.$errors == !v$.address.$errors ? 'success' : 'error'"
+                                            @blur="v$.$touch()"
                                             name="addr1" 
                                             placeholder="〇〇町1-1　〇〇マンション301"
                                         >
+                                        <span class="invalid-feedback" v-for="error of v$.address.$errors" :key="error.$uid">{{ error.$message }}</span>
                                     </div>
                                     <div class="form_box">
                                         <div class="form_headline">
                                             電話番号
                                         </div>
                                         <input type="text" 
+                                            v-model="state.registerForm.phone"
+                                            v-bind:class="v$.phone.$errors == !v$.phone.$errors ? 'success' : 'error'"
+                                            @blur="v$.$touch()"
                                             name="" 
                                             placeholder="00-0000-0000"
                                         >
+                                        <span class="invalid-feedback" v-for="error of v$.phone.$errors" :key="error.$uid">{{ error.$message }}</span>
                                     </div>
                                     <div class="form_box">
                                         <div class="form_headline">
                                         メールアドレス
                                         </div>
                                         <input type="email" 
+                                            v-model="state.registerForm.email"
+                                            v-bind:class="v$.email.$errors == !v$.email.$errors ? 'success' : 'error'"
+                                            @blur="v$.$touch()"
                                             name="" 
                                             placeholder="example@example.com"
                                         >
+                                        <span class="invalid-feedback" v-for="error of v$.email.$errors" :key="error.$uid">{{ error.$message }}</span>
                                     </div>
                                     <div class="form_box">
                                         <div class="form_headline">
                                         パスワード
                                         </div>
                                         <input type="text" 
+                                            v-model="state.registerForm.password"
+                                            v-bind:class="v$.password.$errors == !v$.password.$errors ? 'success' : 'error'"
+                                            @blur="v$.$touch()"
                                             name="" 
                                             placeholder="※半角英数字１５文字以内"
                                         >
+                                        <span class="invalid-feedback" v-for="error of v$.password.$errors" :key="error.$uid">{{ error.$message }}</span>
                                     </div>
 
                                 </div>
@@ -282,11 +315,15 @@ const router = useRouter()
 
 const state = reactive({
     registerForm: {
-        organizationName: '',
-        affiliates: '',
-        name: '',
-        zipCode1: '',
-        zipCode2: '',
+        type: '',
+        organization_company: '',
+        organization_school: '',
+        affiliates_company: '',
+        affiliates_school: '',
+        company_name: '',
+        school_name: '',
+        zip_code_1: '',
+        zip_code_2: '',
         address: '',
         phone: '',
         email: '',
@@ -295,11 +332,14 @@ const state = reactive({
 })
 
 const validations = {
-    organizationName: {required:helpers.withMessage("Organization name is required", required), $autoDirty: true},
-    affiliates: {required:helpers.withMessage("Affiliates is required", required), $autoDirty: true},
-    zipCode1: {required:helpers.withMessage("zipCode1 is required", required), numeric, minLength: minLength(3), maxLength: maxLength(3), $autoDirty: true},
-    zipCode2: {required:helpers.withMessage("zipCode2 is required", required), numeric, minLength: minLength(4), maxLength: maxLength(4), $autoDirty: true},
-    name: {required:helpers.withMessage("Admin name is required", required), $autoDirty: true},
+    organization_company: {required:helpers.withMessage("Organization company is required", required), $autoDirty: true},
+    organization_school: {required:helpers.withMessage("Organization school is required", required), $autoDirty: true},
+    affiliates_company: {required:helpers.withMessage("Affiliates company is required", required), $autoDirty: true},
+    affiliates_school: {required:helpers.withMessage("Affiliates school is required", required), $autoDirty: true},
+    company_name: {required:helpers.withMessage("Company name is required", required), $autoDirty: true},
+    school_name: {required:helpers.withMessage("School name is required", required), $autoDirty: true},
+    zip_code_1: {required:helpers.withMessage("zip code 1 is required", required), numeric, minLength: minLength(3), maxLength: maxLength(3), $autoDirty: true},
+    zip_code_2: {required:helpers.withMessage("zip code 2 is required", required), numeric, minLength: minLength(4), maxLength: maxLength(4), $autoDirty: true},
     address: {required:helpers.withMessage("Address is required", required), $autoDirty: true},
     phone: {required:helpers.withMessage("Phone is required", required), numeric, minLength: minLength(10), maxLength: maxLength(10), $autoDirty: true},
     email: { required:helpers.withMessage("Email is required", required), email, $autoDirty: true},
